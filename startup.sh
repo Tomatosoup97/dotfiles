8102bfe5a9e231c846fe554c395c735d8bd9b72d
Mateusz Urbańczyk <murbanczyk@qed.ai>
2018-07-04 23:42:18 +0200

    Add startup script

---
+++ startup.sh
@@ -0,0 +1,21 @@
+
+i3-msg workspace 3 && sleep 1 && i3-msg exec "i3-sensible-terminal"
+
+i3-msg workspace 3 && sleep 1 && i3-msg exec "i3-sensible-terminal"
+
+i3-msg workspace 5 && sleep 1 && i3-msg exec "i3-sensible-terminal"
+
+i3-msg workspace 5 && sleep 1 && i3-msg exec "i3-sensible-terminal"
+
+# i3-msg workspace 8 && sleep 1 && i3-msg exec "cd /home/mat/Programs/Hubstaff/ && ./HubstaffClient.bin.x86_64"
+
+# i3-msg workspace 8 && sleep 1 && i3-msg exec "redshift"
+
+i3-msg workspace 2 && sleep 1 && i3-msg exec "chromium"
+
+i3-msg workspace 4 && sleep 1 && i3-msg exec "slack"
+
+i3-msg workspace 6 && sleep 1 && i3-msg exec "spotify"
+
+# i3-msg workspace 1 && sleep 1 && i3-msg exec "/home/mat/Programs/pycharm-2017.2.4/bin/pycharm.sh"
+
