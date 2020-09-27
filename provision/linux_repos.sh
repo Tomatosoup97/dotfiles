f1b75a8c16692d978d4d045a6c05edd24c4ddbd9
Mateusz Urbańczyk <mu@qed.ai>
2020-09-27 16:32:14 +0200

    Enable RPM fusion

--- provision/linux_repos.sh
+++ provision/linux_repos.sh
@@ -7,3 +7,6 @@ sudo dnf install -y fedora-workstation-repositories
 # Flatpak
 sudo dnf install -y flatpak
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+# RPM Fusion
+sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
