4e829e0ea0749077e5db896e80f9eb4f8798c342
Mateusz Urbańczyk <mu@qed.ai>
2020-10-08 09:38:28 +0200

    [provision] Install Coq

---
+++ provision/coq.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+opam repo add coq-released https://coq.inria.fr/opam/released
+opam repository add coq-released --set-default
+opam install -y coq
+
+sudo dnf install -y cairo-devel gtk3-devel gtksourceview3-devel
+opam install -y coqide
