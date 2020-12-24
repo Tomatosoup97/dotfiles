22b302dd05d715262754bb13e692037ba67e6c5e
Mateusz Urbańczyk <mu@qed.ai>
2020-12-24 11:19:14 +0100

    [provision] Install elasticsearch

--- provision/databases.sh
+++ provision/databases.sh
@@ -7,3 +7,18 @@ sudo postgresql-setup --initdb --unit postgresql
 
 # Redis
 sudo dnf -y install redis
+
+# Elasticsearch
+# sudo cp ./templates/elasticsearch.repo /etc/yum.repos.d/
+# sudo dnf install -y --enablerepo=elasticsearch elasticsearch
+
+
+# Start databases
+# sudo systemctl start postgresql
+# sudo systemctl start redis
+# sudo systemctl start elasticsearch
+
+# Enable databases
+# sudo systemctl start postgresql
+# sudo systemctl start redis
+# sudo systemctl start elasticsearch
