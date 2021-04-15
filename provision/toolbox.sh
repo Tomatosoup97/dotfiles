16b9392837946f1c87aaee0949c439549a7957ec
Mateusz Urbańczyk <mu@qed.ai>
2021-04-15 08:56:51 +0200

    [provision] Install unrar

--- provision/toolbox.sh
+++ provision/toolbox.sh
@@ -48,3 +48,8 @@ sudo dnf install -y mediawriter
 
 # vifm
 sudo dnf install -y vifm
+
+# unrar
+sudo dnf install -y  https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf install -y https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf install -y unrar
