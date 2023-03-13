bad9f64be15a4a0278744eb3b80031cc3cde27e0
Mateusz Urbańczyk <mu@qed.ai>
2023-03-13 17:11:48 +0100

    [provision] Add mypy required lib

--- provision/python.sh
+++ provision/python.sh
@@ -5,6 +5,7 @@ sudo dnf install -y python-pip
 sudo dnf install -y python3-virtualenv
 sudo pip install -r requirements.txt
 sudo dnf install -y python-devel
+sudo dnf install -y libdmtx  # mypy requires that
 
 # Python build deps
 sudo dnf install -y make gcc zlib-devel bzip2 bzip2-devel readline-devel sqlite sqlite-devel openssl-devel tk-devel libffi-devel
